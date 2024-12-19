a64e335e000a9fa432886d1af7e4b74b512af3ff
Ashraf <ashraf.moham456@gmail.com>
2024-12-19 22:20:25 +0200

    fix mapping

--- app/src/main/java/com/example/solarustask/features/transactions/data/WebServices.kt
+++ app/src/main/java/com/example/solarustask/features/transactions/data/WebServices.kt
@@ -1,11 +1,10 @@
 package com.example.solarustask.features.transactions.data
 
-import retrofit2.Response
 import retrofit2.http.GET
 
 interface WebServices {
 
     @GET("getTransactions")
-    suspend fun getTransactions(): Response<TransactionsResponse>
+    suspend fun getTransactions(): TransactionsResponse
 
 }
